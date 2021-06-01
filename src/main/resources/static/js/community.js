3c6d3bdff2bfd4bf1a38fdc0bb71b04ebb14a030
huyf <724826175@qq.com>
2021-06-01 10:55:12 +0800

    添加回复页面

---
+++ src/main/resources/static/js/community.js
@@ -0,0 +1,24 @@
+
+
+function post() {
+    var questionId = $("#question_id").val();
+    var content = $("#comment_content").val();
+    $.ajax({
+        type: "POST",
+        url: "/community/comment",
+        contentType: 'application/json',
+        data: JSON.stringify({
+            "parentId": questionId,
+            "content": content,
+            "type": 1
+        }),
+        success: function (response) {
+            if (response.code == 200) {
+                $("comment_content").hide();
+            } else {
+                alert(response.message);
+            }
+        },
+        dataType: "json"
+    });
+}
